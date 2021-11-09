8aec69d1983c55b2321f63eb5a264be9c65597e5
Matthew Pompilio <mattflynnpomp@aol.com>
2021-11-09 16:45:54 -0500

    added posting data

--- server.js
+++ server.js
@@ -1,17 +1,74 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 
-const { notes } = require('./db/db');
-
+const { notes } = require('./db/db.json');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+  if (query.title) {
+    filteredResults = filteredResults.filter(note => note.title === query.title);
+  }
+  if (query.text) {
+    filteredResults = filteredResults.filter(note => note.text === query.text);
+  }
+  return filteredResults;
+}
 
 
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
 
-  app.get('/api/notes', (req, res) => {
-    res.json(notes);
+  return note;
+}
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  }
+  return true;
+}
+
+
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+  app.post('/api/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('This note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
   });
 
 
+
+
 app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+  console.log(`API server now on port ${PORT}!`);
 });
+
