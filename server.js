63f781a9bb60b9603d4a5dad07dea5f849194ac6
Matthew Pompilio <mattflynnpomp@aol.com>
2021-11-08 19:36:54 -0500

    not sure why json isnt displaying

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const { notes } = require('./db/db');
+
+const app = express();
+
+
+
+
+  app.get('/api/notes', (req, res) => {
+    res.json(notes);
+  });
+
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
